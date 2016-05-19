b1187a6796097e3782e94911fa5135d80ba871d4
marcelomata <marcelo.oliveira@ufrgs.br>
2016-05-19 01:01:36 -0300

    Testing drawing in BufferedImages

--- TestsImages/src/Main.java
+++ TestsImages/src/Main.java
@@ -1,3 +1,7 @@
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -19,9 +23,23 @@ public class Main {
 			}
 		}
 		
-		TrackingCells2D tracking = new TrackingCells2D(movie);
-		tracking.trackCells();
+//		TrackingCells2D tracking = new TrackingCells2D(movie);
+//		tracking.trackCells();
 		
+		BufferedImage image = movie.get(0);
+//		
+		Graphics2D g2d = image.createGraphics();
+	    g2d.setBackground(Color.WHITE);
+	    g2d.setColor(Color.BLACK);
+	    BasicStroke bs = new BasicStroke(2);
+	    g2d.setStroke(bs);
+	    g2d.drawLine(0, 0, 150, 150);
+		
+		try {
+			ImageIO.write(image, "BMP", new File("./Tests/testDraw.bmp"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
