c44e98162f47e1e20daba7a072730f85f7bf4c76
marcelodmo <marcelodmo@BII-LC7>
2016-05-25 12:54:37 +0800

    Tracking3D initialization

--- Tracking3D/src/Main.java
+++ Tracking3D/src/Main.java
@@ -1,9 +1,8 @@
-import java.io.File;
-
 public class Main {
 	
 	public static void main(String[] args) {
-		File image = new File("/home/Documents/data/droso-seg.tif");	
+		PluginTracking tracking = new PluginTracking();
+		tracking.run("");
 	}
 
 }
---
+++ Tracking3D/src/PluginTracking.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+import ij.ImagePlus;
+import ij.io.Opener;
+import ij.plugin.PlugIn;
+import mcib3d.geom.Objects3DPopulation;
+
+public class PluginTracking implements PlugIn {
+	@Override
+	public void run(String arg) {
+		File image = new File("/home/marcelodmo/Documents/data/droso-seg.tif");	
+		Opener open = new Opener();
+		ImagePlus imp = open.openImage(image.getAbsolutePath());
+		Objects3DPopulation population = new Objects3DPopulation(imp);
+		System.out.println(population.getNbObjects());
+	}
+}
