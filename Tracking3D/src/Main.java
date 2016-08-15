98a846c44bd71660710dbf8da3069111a8736872
marcelodmo <marcelodmo@BII-LC7>
2016-08-15 18:58:10 +0800

    Testing datasets.

--- Tracking3D/src/Main.java
+++ Tracking3D/src/Main.java
@@ -6,13 +6,13 @@ public class Main {
 		
 //		PluginTracking tracking = new PluginTracking("/home/marcelodmo/Documents/data/Challenge/N2DH-GOWT1/");
 //		PluginTracking tracking = new PluginTracking("/home/marcelodmo/Documents/data/Challenge/N2DH-GOWT1-mini/");
-//		PluginTracking tracking = new PluginTracking("/home/marcelodmo/Documents/data/Challenge/N2DH-SIM/");
+		PluginTracking tracking = new PluginTracking("/home/marcelodmo/Documents/data/Challenge/N2DH-SIM/");
 //		PluginTracking tracking = new PluginTracking("/home/marcelodmo/Documents/data/Challenge/N2DL-HeLa/");
 //		PluginTracking tracking = new PluginTracking("/home/marcelodmo/Documents/data/Challenge/N3D-CHO-2/N3DH-CHO-testing/");
 //		PluginTracking tracking = new PluginTracking("/home/marcelodmo/Documents/data/Challenge/N3D-CHO-2/N3DH-CHO-training/");
 //		PluginTracking tracking = new PluginTracking("/home/marcelodmo/Documents/data/Challenge/Raw1-amal/");
 //		PluginTracking tracking = new PluginTracking("/home/marcelodmo/Documents/data/Challenge/Raw2-amal/");
-		PluginTracking tracking = new PluginTracking("/home/marcelodmo/Documents/data/Challenge/Test110/");
+//		PluginTracking tracking = new PluginTracking("/home/marcelodmo/Documents/data/Challenge/Test110/");
 //		PluginTracking tracking = new PluginTracking("/home/marcelodmo/Documents/data/Challenge/Test1/");
 //		PluginTracking tracking = new PluginTracking("/home/marcelodmo/Documents/data/Challenge/Test2/");
 //		PluginTracking tracking = new PluginTracking("/home/marcelodmo/Documents/data/Challenge/Test3");
